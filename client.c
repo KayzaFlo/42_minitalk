74bac0f1740ead9320c511db67ae1196feb9363f
fgeslin <fgeslin@student.42quebec.com>
2023-02-06 17:50:56 -0500

    it's alive

--- client.c
+++ client.c
@@ -6,13 +6,57 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:57:35 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/01/31 13:33:33 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/02/06 17:38:00 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+void	send_bit(int c, int b, int pid)
+{
+	if (c >= b)
+	{
+		kill(pid, SIGUSR1);
+		c -= b;
+	printf("1");
+	}
+	else
+	{
+		kill(pid, SIGUSR2);
+	printf("0");
+	}
+	usleep(10);
+	if (b <= 1)
+		return ;
+	send_bit(c, b / 2, pid);
+}
+
 int	main(int argc, char const *argv[])
 {
+	pid_t	server_pid;
+	pid_t	pid;
+	char	*to_send;
+	int		len;
+
 	if (argc != 3 || !argv)
 		return (0);
+	server_pid = atoi(argv[1]);
+	pid = getpid();
+	len = 0;
+	while (argv[2][len])
+		len++;
+	send_bit(len, 128, server_pid);
+	send_bit(0, 128, server_pid);
+	to_send = (char *)argv[2];
+	while (*to_send)
+	{
+		send_bit(*to_send, 128, server_pid);
+		to_send++;
+	}
+		send_bit(*to_send, 128, server_pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,32 +6,74 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:57:33 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/01/31 13:34:04 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/02/06 17:48:44 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void	*g_input;
 
 void	oi(int signum)
 {
-	printf("signal received\n");
+	static int	nb = 0;
+	static int	count = 0;
+	static int	i = 0;
+
+	if (((char *)g_input)[i] <= 0)
+		i = 0;
+	nb = nb << 1;
 	if (signum == SIGUSR1)
-		printf("SIGUSR1\n");
+		nb++;
+	count = (count + 1) % 8;
+	if (!count)
+	{
+		nb += '0';
+		write(1, &nb, 1);
+		((char *)g_input)[i] = nb;
+		nb = 0;
+		i++;
+	}
+}
+
+static void	init_sigs(struct sigaction *sa)
+{
+	sa->sa_handler = oi;
+	sigemptyset(&sa->sa_mask);
+	sa->sa_flags = SA_RESTART; /* Restart functions if interrupted by handler */
+	if (sigaction(SIGUSR1, sa, NULL) == -1)
+		exit (-1);
+	if (sigaction(SIGUSR2, sa, NULL) == -1)
+		exit (-1);
 }
 
 int	main(int argc, char const *argv[])
 {
-	pid_t	pid;
+	struct sigaction	sa;
+	pid_t				pid;
+	int					i;
+	int					len;
 
 	if (argc != 1 || !argv)
 		return (0);
-	signal(SIGUSR1, oi);
-	signal(SIGUSR2, oi);
+	init_sigs(&sa);
 	pid = getpid();
 	printf("pid: %d\n", pid);
-	kill(pid, SIGUSR1);
-	kill(pid, SIGUSR2);
+	g_input = malloc(2);
+	((char *)g_input)[0] = -1;
+	((char *)g_input)[1] = -1;
+	while (((char *)g_input)[1])
+		pause();
+	len = (int)((char *)g_input)[1];
+	free (g_input);
+	g_input = malloc(len + 1);
+	i = -1;
+	while (++i < len + 1)
+		((char *)g_input)[i] = -1;
+	while (((char *)g_input)[len])
+		pause();
 	return (0);
 }
