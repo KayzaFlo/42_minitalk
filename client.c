c8e15399252aff79f2941229ece3165f320d3f68
fgeslin <fgeslin@student.42quebec.com>
2023-02-10 17:07:10 -0500

    crash when too fast

--- client.c
+++ client.c
@@ -6,11 +6,13 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:57:35 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/02/07 16:59:57 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/02/10 16:43:01 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int	g_sig;
 
 void	send_bit(uint32_t c, size_t size, int pid)
 {
@@ -23,23 +25,28 @@ void	send_bit(uint32_t c, size_t size, int pid)
 	{
 		kill(pid, SIGUSR2);
 	}
-	usleep(40);
+	usleep(400);
+	// while (!g_sig)
+	// 	pause();
+	// g_sig = 0;
 	if (size <= 1)
 		return ;
 	send_bit(c, size / 2, pid);
 }
 
-static void	inputreceived(int signum)
+static void	handler(int signum)
 {
 	if (signum == SIGUSR1)
 		ft_printf(KYEL"-"KNRM"message received"KYEL"-\n"KNRM);
+	//if (signum == SIGUSR2)
+		g_sig = 1;
 }
 
 static void	init_sigs(struct sigaction *sa)
 {
-	sa->sa_handler = inputreceived;
+	sa->sa_handler = handler;
 	sigemptyset(&sa->sa_mask);
-	sa->sa_flags = SA_RESTART;
+	sa->sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, sa, NULL) == -1)
 		exit (-1);
 	if (sigaction(SIGUSR2, sa, NULL) == -1)
@@ -56,15 +63,12 @@ int	main(int argc, char const *argv[])
 		return (0);
 	server_pid = atoi(argv[1]);
 	init_sigs(&sa);
-	send_bit(getpid(), 2147483648, server_pid);
-	send_bit(ft_strlen(argv[2]), 2147483648, server_pid);
-	send_bit(0, 2147483648, server_pid);
 	to_send = (char *)argv[2];
 	while (*to_send)
 	{
 		send_bit((unsigned char)*to_send, 128, server_pid);
 		to_send++;
 	}
-	send_bit(*to_send, 128, server_pid);
+	send_bit('\0', 128, server_pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,61 +6,46 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:57:33 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/02/09 12:42:25 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/02/10 16:40:34 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	*g_input;
+pid_t	cur_client;
 
-static void	handler_uint(int sig, siginfo_t *info, void *ucontext)
-{
-	static uint32_t	c = 0;
-	static int		count = 0;
-	static int		i = 0;
-
-	(void)ucontext;
-	(void)info;
-	c = c << 1;
-	if (sig == SIGUSR1)
-		c++;
-	count = (count + 1) % 32;
-	if (!count)
-	{
-		((uint32_t *)g_input)[i] = c;
-		if (!c)
-			i = 0;
-		else
-			i++;
-		c = 0;
-	}
-}
-
-static void	handler_char(int sig, siginfo_t *info, void *ucontext)
+static void	handler_byte(int sig, siginfo_t *info, void *ucontext)
 {
 	static char	c = 0;
 	static int	count = 0;
-	static int	i = 0;
+	static char	*str = "";
 
 	(void)ucontext;
-	(void)info;
 	c = c << 1;
+	// ft_printf(sig == SIGUSR1 ? "1" : "0");
 	if (sig == SIGUSR1)
 		c++;
 	count = (count + 1) % 8;
 	if (!count)
 	{
-		((char *)g_input)[i] = c;
-		if (!c)
-			i = 0;
-		else
-			i++;
+		str = ft_strjoin(str, &c);
+		if (!c && *str)
+		{
+			ft_printf("%s\n", str);
+			free (str);
+			str = "";
+	// usleep(400);
+			kill(info->si_pid, SIGUSR1);
+			// cur_client = 0;
+			// return ;
+		}
 		c = 0;
 	}
+	// usleep(400);
+	kill(info->si_pid, SIGUSR2);
 }
 
-static void	init_sigs(struct sigaction *sa, void f(int, siginfo_t, void*))
+static void	init_sigs(struct sigaction *sa, void f(int, siginfo_t*, void*))
 {
 	sigemptyset(&sa->sa_mask);
 	sa->sa_flags = SA_SIGINFO;
@@ -71,43 +56,16 @@ static void	init_sigs(struct sigaction *sa, void f(int, siginfo_t, void*))
 		exit (-1);
 }
 
-void	waiting(struct sigaction *sa)
-{
-	pid_t	client_pid;
-	int		len;
-
-	init_sigs(sa, handler_uint);
-	g_input = calloc(3, sizeof(uint32_t));
-	ft_memset(g_input, -1, 3 * sizeof(uint32_t));
-	while (((uint32_t *)g_input)[2])
-		pause();
-	client_pid = ((uint32_t *)g_input)[0];
-	len = ((uint32_t *)g_input)[1];
-	init_sigs(sa, handler_char);
-	g_input = realloc(g_input, (len + 1) * sizeof(char));
-	ft_memset(g_input, -1, (len + 1) * sizeof(char));
-	while (((char *)g_input)[len])
-		pause();
-	ft_printf("%s\n", (char *)g_input);
-	kill(client_pid, SIGUSR1);
-}
-
 int	main(int argc, char const *argv[])
 {
 	struct sigaction	sa;
-	pid_t				pid;
 
 	if (argc != 1 || !argv)
 		return (0);
-	pid = getpid();
-	printf(KYEL KBLD "pid:" KWHT " %d" KNRM "\n", pid);
-	while (pid)
-	{
-		waiting(&sa);
-		if (!ft_strncmp((char *)g_input, "quit", 4))
-			break ;
-		free (g_input);
-	}
-	free (g_input);
+	// cur_client = 0;
+	printf(KYEL KBLD "pid:" KWHT " %d" KNRM "\n", getpid());
+	init_sigs(&sa, handler_byte);
+	while (1)
+		pause();
 	return (0);
 }
